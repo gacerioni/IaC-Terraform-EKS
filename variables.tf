6e7c4a27632985edb9976495488ee5ed18400692
Gabriel Cerioni <gacerioni@gmail.com>
2021-11-09 20:34:07 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+#EKS variables.
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "kube_config_path" {
+  default = "/Users/gacerioni/.kube/config"
+}
+
+variable "kube_config_context" {
+  default = "arn:aws:eks:sa-east-1:915632791698:cluster/gabs-quick-terraform-lab"
+}
+
+variable "eks_cluster_name" {
+  default = "arn:aws:eks:sa-east-1:915632791698:cluster/gabs-quick-terraform-lab"
+}
+
+variable "kubernetes-namespace" {
+    default = "gabs-exec-ns"
+}
