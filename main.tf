0d26b104e020b5fd614de7c6ffaa413d498a8da1
Gabriel Cerioni <gacerioni@gmail.com>
2021-11-09 20:33:51 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "kubernetes" {
+  config_path    = var.kube_config_path
+  config_context = var.kube_config_context
+}
+
+resource "kubernetes_namespace" "test_auth_namespace" {
+  metadata {
+    name = var.kubernetes-namespace
+  }
+}
+
+resource "kubernetes_config_map" "example_richard_gabs" {
+  metadata {
+    name = "my-tf-lab-config"
+    namespace = "kube-system"
+  }
+
+  data = {
+    api_host             = "myhost:443"
+    db_host              = "dbhost:5432"
+  }
+}
