8d855a13976ba5801e51101ee41186b7c0c1de2a
Gabriel Cerioni <gacerioni@gmail.com>
2021-11-09 21:53:19 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "kubernetes" {
-  #config_path    = var.kube_config_path
-  #config_context = var.kube_config_context
+  config_path    = var.kube_config_path
+  config_context = var.kube_config_context
 }
 
 resource "kubernetes_namespace" "test_auth_namespace" {
@@ -16,6 +16,6 @@ resource "kubernetes_config_map" "aws_auth" {
   }
 
   data = {
-    mapRoles = "- groups:\n  - system:bootstrappers\n  - system:nodes\n  rolearn: arn:aws:iam::915632791698:role/gabs_eks_workernodes\n  username: system:node:{{EC2PrivateDNSName}}\n- groups:\n  - system:masters\n  rolearn: arn:aws:iam::915632791698:role/ec2_delegate_poweruser_role\n  username: arn:aws:iam::915632791698:role/ec2_delegate_poweruser_role\n"
+    mapRoles = "TEST"
   }
 }
